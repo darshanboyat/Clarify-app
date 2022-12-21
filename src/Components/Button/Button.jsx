b422abfec4edd791e3f73c034256c80711e2cbe1
darshanboyat <106928085+darshanboyat@users.noreply.github.com>
2022-12-21 15:12:28 +0530

    first commit

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -9,7 +9,7 @@ export default function BasicButtons() {
   return (
     <Stack spacing={2} direction="row" sx={{ml: '11.2%'}}>
       <Link to = "/signin" className="navbar-btn"><Button variant="outlined" sx={{color: 'black', border: '.1px solid rgb(198, 198, 198)', borderRadius: 2, fontWeight: 'bold', padding: '8px 24px', fontSize: 16, fontFamily: 'Gilroy-ExtraBold', textTransform: 'capitalize'}}>Sign in</Button></Link>
-      <Link to = "/signup" className="navbar-btn"><Button variant="contained" sx={{backgroundColor: '#0c4d71', borderRadius: 2, fontWeight: 'bold', padding: '8px 24px', fontSize: 16, fontFamily: 'Gilroy-ExtraBold', textTransform: 'capitalize'}}>Sign up</Button></Link>
+      <Link to = "/auth/register" className="navbar-btn"><Button variant="contained" sx={{backgroundColor: '#0c4d71', borderRadius: 2, fontWeight: 'bold', padding: '8px 24px', fontSize: 16, fontFamily: 'Gilroy-ExtraBold', textTransform: 'capitalize'}}>Sign up</Button></Link>
       <Link to = "/" className="navbar-btn"><Button variant="outlined" sx={{color: 'black', border: '.1px solid rgb(198, 198, 198)', borderRadius: 2, padding: '8px 24px', fontSize: 14}}><LanguageIcon/>EN</Button></Link>
     </Stack>
   );
